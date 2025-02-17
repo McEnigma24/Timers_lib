ca6c5bdc4c446a011aeb21290eb9a8c352d360af
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-02-18 00:13:45 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -1,12 +1,26 @@
 #include "__preprocessor__.h"
 #include "__time_stamp__.h"
 
+#include "Nano_Timer.h"
+
 #ifdef BUILD_EXECUTABLE
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
     UTILS::clear_terminal();
     time_stamp("It just works");
+
+    Timer timer;
+
+    for (int i{}; i < 25; i++)
+    {
+        int random = rand() % 3000;
+
+        timer.start();
+        sleep(random);
+        timer.end();
+        timer.log();
+    }
 
     return 0;
 }
